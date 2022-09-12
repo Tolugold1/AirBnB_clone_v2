28d2d13f34f86841be50469e31d8c2235b21775b
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-12 14:23:26 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -22,6 +22,6 @@ class State(BaseModel):
             """getter"""
             list_of_city = []
             for city in models.storage.all(City).values():
-                if city.State_id == self.id:
+                if city.state_id == self.id:
                     list_of_city.append(city)
             return list_of_city
