ac823ad465fc414887b95862cbf2bac8a3d75f83
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-12 13:43:55 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""State class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+import os
+import models
+from models.city import City
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """state class"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              Cascade="all delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """getter"""
+            list_of_city = []
+            for lt in models.storage.all(City).values():
+                if City.State_id == self.id:
+                    list_of_city.append(lt)
+            return list_of_city
