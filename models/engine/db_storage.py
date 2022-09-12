c300738798c65cf5efaf9458d5458e5ec3518e32
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-12 15:36:04 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,18 +21,17 @@ class DBStorage():
     __session = "None"
 
     def __init__(self):
-        """Initializing"""
-        MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        """Instantiation of DBStorage class
+        """
+        MySQL_user = getenv('HBNB_MYSQL_USER')
+        MySQL_pwd = getenv('HBNB_MYSQL_PWD')
+        MySQL_host = getenv('HBNB_MYSQL_HOST')
+        MySQL_db = getenv('HBNB_MYSQL_DB')
         MySQL_env = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      ''.format(
-            MYSQL_USER, MYSQL_PWD,
-        MYSQL_HOST, MYSQL_DB), pool_pre_ping=True)
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            MySQL_user, MySQL_pwd, MySQL_host, MySQL_db), pool_pre_ping=True)
         Base.metadata.create_all(self.__engine)
-        if MySQL_env == 'test':
+        if MySQL_env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
