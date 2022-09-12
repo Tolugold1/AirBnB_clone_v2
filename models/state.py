d2d334839b1ce39bfa3c00254717562be8abf0ec
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-12 14:46:30 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -8,7 +8,7 @@ import models
 from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """state class"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
