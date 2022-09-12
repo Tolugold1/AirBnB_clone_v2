dd30b97e384913cd2285ba222de59bb6ddba91b1
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-12 14:01:04 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -26,6 +26,7 @@ class DBStorage():
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        MySQL_env = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
                                       ''.format(
             HBNB_MYSQL_USER, HBNB_MYSQL_PWD,
