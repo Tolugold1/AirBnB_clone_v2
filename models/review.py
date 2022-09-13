60b33d2ed5cb36e44e1c61007620203290472244
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-13 11:10:35 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Integer, Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel,Base):
-    """ Review classto store review information """
+class Review(BaseModel, Base):
+    """ Review class to store review information """
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
-    user = relationship('User', backref="reviews")
-    place = relationship('Place', backref='reviews')
