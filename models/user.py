961a9f1fbe133e8dedb902e99c0010f99f836c6c
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-13 11:07:03 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -5,12 +5,13 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel,Base):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship('Place', back_populates="user")
-    reviews = relationship('Reviews', back_populates="user")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref="user",
+                          cascade='all, delete-orphan')
+    reviews = relationship('Reviews', backref="user")
