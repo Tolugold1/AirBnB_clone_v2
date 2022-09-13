fba155788abe6a7f19bbf3ab43a99c0f9829ca13
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-13 11:08:24 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel,Base
-from sqlalchemy import Integer, Column, String, ForeignKey,Float
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Integer, Column, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
+from models.review import Review
+from models.amenity import Amenity
+from sqlalchemy import *
+import os
+
+metadata = Base.metadata
+
+place_amenity = Table('place_amenity', metadata,
+                      Column(
+                          'place_id', String(60),
+                          ForeignKey('places.id'),
+                          primary_key=True, nullable=False),
+                      Column(
+                          'amenity_id', String(60),
+                          ForeignKey('amenities.id'),
+                          primary_key=True, nullable=False)
+                      )
 
 
 class Place(BaseModel,Base):
@@ -11,14 +29,39 @@ class Place(BaseModel,Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
-    user = relationship('User', back_populates='places')
-    cities = relationship('City', back_populates='places')
-    reviews = relationship('Review', back_populates='place')
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                 backref="places", viewonly=False)
+        reviews = relationship('Review', backref='places',
+                           cascade='all, delete-orphan')
+    @property
+    def reviews(self):
+        """Get the lists of review"""
+        lists_of_review = []
+        for revw in models.storage.all(Review).values():
+            if revw.place_id == self.id:
+                lists_of_review.append(revw)
+        return lists_of_review
+
+    @property
+    def amenities(self):
+        """getter: returns the list of Amenity"""
+        lists_of_Amenity = []
+        for amnt in models.storage.all(Amenity).values():
+            if amnt.amenity_ids == self.id:
+                lists_of_Amenity.append(amnt)
+        return lists_of_Amenity
+
+    @amenities.setter
+    def amenities(self, objct):
+        """amenities setter"""
+        if type(objct) == 'Amenity':
+            self.amenity_ids.append(objct.id)
