4a9a128eb39fb6f4198ec861cac038dd4b5cd930
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-29 21:37:19 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/releases/ /data/web_static/shared/
 echo "Welcome page" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/current /data/web_static/releases/test/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i /listen 80 default_server;/a location /hbnb_static { alias //data/web_static/current/; } /etc/nginx/sites-available/default
 service nginx restart
