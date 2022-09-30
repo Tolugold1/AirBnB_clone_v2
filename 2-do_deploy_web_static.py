c87a8bc3f032ffd1224fc0baada304042766a8af
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-30 03:18:17 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,25 +29,21 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """
-    Fabric script (based on the file 1-pack_web_static.py)
-    that distributes an archive to your web servers,
-    using the function do_deploy
-    """
+    """ Distribute an archive to the web servers """
     if not isfile(archive_path):
         return False
-    put('{}'.format(archive_path), '/tmp/')
+    put(archive_path, '/tmp/')
     archive = archive_path.replace('.tgz', '')
     archive = archive.replace('versions/', '')
-    run("mkdir -p /data/web_static/releases/{}/".format(archive))
-    run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(
-        archive, archive))
-    run("rm /tmp/{}.tgz".format(archive))
+    run('mkdir -p /data/web_static/releases/{}/'.format(archive))
+    run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
+        .format(archive, archive))
+    run('rm /tmp/{}.tgz'.format(archive))
     run('mv /data/web_static/releases/{}/web_static/* '.format(archive) +
         '/data/web_static/releases/{}/'.format(archive))
     run('rm -rf /data/web_static/releases/{}/web_static'.format(archive))
     run('rm -rf /data/web_static/current')
-    run("ln -s /data/web_static/releases/{}/ "
-        "/data/web_static/current".format(archive))
-    print("New version deployed!")
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+        .format(archive))
+    print('New version deployed!')
     return True
