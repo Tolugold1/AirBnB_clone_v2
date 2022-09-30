ece7a8aa064f47534a4a4e5d0345c5e19b83c82c
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-30 09:12:28 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,8 +13,9 @@ def do_pack():
     """.tgz generator function"""
     date_now = datetime.now()
     file_name = 'web_static_{}{}{}{}{}{}.tgz'.format(
-        str(date_now.year), str(date_now.month), str(date_now.day),
-        str(date_now.hour), str(date_now.minute), 
+        str(date_now.year), str(date_now.month), 
+        str(date_now.day),
+        str(date_now.hour), str(date_now.minute),
         str(date_now.second))
     local("mkdir -p  versions")
     archive = local('tar -cvzf versions/{} web_static'.format(
