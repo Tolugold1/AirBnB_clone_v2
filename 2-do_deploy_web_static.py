85cae5d839bd2d04e7feb1410bfb36c8c9098013
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-29 18:25:54 -0700

    update

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive
+from the contents of the web_static
+"""
+
+from fabric.api import *
+from datetime import datetime
+from os.path import exists
+
+
+env.user = 'ubuntu'
+env.hosts = ['3.239.86.158', '34.239.166.29']
+
+
+def do_pack():
+    """.tgz generator function"""
+    date_now = datetime.now()
+    file_name = 'web_static_{}{}{}{}{}{}.tgz'.format(
+        str(date_now.year), str(date_now.month), str(date_now.day),
+        str(date_now.hour), str(date_now.minute),
+        str(date_now.second))
+    local("mkdir -p  versions")
+    archive = local('tar -cvzf versions/{} web_static'.format(
+        file_name))
+    if archive.failed:
+        return None
+    return 'versions/{}'.format(file_name)
+
+
+def do_deploy(archive_path):
+    """
+    Fabric script (based on the file 1-pack_web_static.py)
+    that distributes an archive to your web servers,
+    using the function do_deploy
+    """
+    file_path = exists(archive_path)
+    if not file_path:
+        return file_path
+    put('{}'.format(archive_path), '/tmp/')
+    archive__ = archive_path.split('.')
+    archive_ = archive__[0].split("/")
+    archive = archive_[1]
+    run("mkdir -p /data/web_static/releases/{}/".format(archive))
+    run("tar -xzf /tmp/{}.tgz -c /data/web_static/releases/{}/".format(
+        archive, archive))
+    run("rm /tmp/{}.tgz".format(archive))
+    run('mv /data/web_static/releases/{}/web_static/* '.format(archive) +
+        '/data/web_static/releases/{}/'.format(archive))
+    run('rm -rf /data/web_static/releases/{}/web_static'.format(archive))
+    run('rm -rf /data/web_static/current')
+    run("ls -s /data/web_static/releases/{}/ "
+        "/data/web_static/current".format(archive))
+    print("New version deployed!")
+    return True
