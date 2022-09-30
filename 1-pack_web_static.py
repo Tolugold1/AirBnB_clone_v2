cb16d2b94b9ee8ebe19ce220d36f0b541d1e45e3
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-09-30 01:14:19 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,10 +13,11 @@ def do_pack():
     date_now = datetime.now()
     file_name = 'web_static_{}{}{}{}{}{}.tgz'.format(
         str(date_now.year), str(date_now.month), str(date_now.day),
-        str(date_now.hour), str(date_now.minute), str(date_now.second))
+        str(date_now.hour), str(date_now.minute), 
+        str(date_now.second))
     local("mkdir -p  versions")
-    archive = local('tar -cvzf versions/{} web_static'.format(file_name))
+    archive = local('tar -cvzf versions/{} web_static'.format(
+        file_name))
     if archive.failed:
         return None
     return 'versions/{}'.format(file_name)
-
