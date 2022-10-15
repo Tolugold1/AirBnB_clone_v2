de77e2e3aa89428e294b95f41d641ac8bf37c39b
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-15 23:14:38 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,10 +17,10 @@ class BaseModel:
     """class BaseModel that defines all methods"""
     id = Column(String(60), unique=True, nullable=False, 
                 primary_key=True)
-    created_at = Column(sa.DateTime, nullable=False, 
-                        default=datetime.utcnow())
-    updated_at = Column(sa.DateTime, nullable=False, 
-                        default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, 
+                        default=datetime.datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, 
+                        default=datetime.datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Initializing BaseModel"""
@@ -30,6 +30,8 @@ class BaseModel:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
+            if not self.id:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
