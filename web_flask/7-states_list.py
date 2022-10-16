92dde97f8fdf286a6c51f61e90114fa9fe452070
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-16 05:45:32 -0700

    update

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from sre_parse import State
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+@app.route('/states_list', 'strict_slashes=False')
+def states_list(states=None):
+    """display list of states"""
+    return render_template('7-states_list.html', states=storage.all(State).values())
+
+
+@app.teardown_appcontext
+def remove_session(response_or_exc):
+    """Remove the surrent session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
