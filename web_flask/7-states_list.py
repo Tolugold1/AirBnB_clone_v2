5b7d05cb0cbf55ad11663cad5f84b4a92a9a3c18
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-16 14:56:39 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/states_list', strict_slashes=False)
 def states_list(states=None):
